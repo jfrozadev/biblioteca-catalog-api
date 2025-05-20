6600dedccb69a55827e17e1cf41a9fab2e54e85a
jaderfroza <jaderfroza@gmail.com>
2025-05-20 01:30:28 +0000

    adjustments

--- src/biblioteca-catalog.Application/Commands/Autor/DeleteAutor/DeleteAutorCommand.cs
+++ src/biblioteca-catalog.Application/Commands/Autor/DeleteAutor/DeleteAutorCommand.cs
@@ -1,6 +1,3 @@
-namespace biblioteca_catalog.Application.Commands.Autor.DeleteAutor
-{
-    public record DeleteAutorCommand(int CodAu) : IRequest<Unit>;
-    {
-}
-}
+namespace biblioteca_catalog.Application.Commands.Autor.DeleteAutor;
+
+public record DeleteAutorCommand(int CodAu) : IRequest<Unit>;
